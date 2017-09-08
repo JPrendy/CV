bd026aa38e67269c13bd6817550208232cec8233
James Prendergast <jamprendergast@gmail.com>
2017-09-08 13:28:26 +0100

    the addition of the new website

---
+++ sw.js
@@ -0,0 +1,33 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('CV_Website_version_2').then(function(cache) {
+     return cache.addAll([
+       'index.html',
+       'index.html?homescreen=1',
+       '?homescreen=1',
+       'manifest.json',
+       'projects.html',
+       'projects.html?homescreen=1',
+       '?homescreen=1',
+       'script.js',
+       'style.css'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+
+console.log(event.request.url);
+
+event.respondWith(
+
+caches.match(event.request).then(function(response) {
+
+return response || fetch(event.request);
+
+})
+
+);
+
+})
